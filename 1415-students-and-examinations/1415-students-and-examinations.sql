b2d04aed5a9e6a73edd5abd92af83eb27019b2cf
Ayumudayo <129680598+Ayumudayo@users.noreply.github.com>
2024-09-11 10:51:45 +0900

    Time: 750 ms (91.11%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT S.student_id, S.student_name, Sub.subject_name,
+       SUM(CASE WHEN E.subject_name IS NOT NULL THEN 1 ELSE 0 END) AS attended_exams
+FROM Students S
+JOIN Subjects Sub
+LEFT JOIN Examinations E
+    ON S.student_id = E.student_id AND Sub.subject_name = E.subject_name
+GROUP BY S.student_id, S.student_name, Sub.subject_name
+ORDER BY S.student_id ASC, Sub.subject_name ASC;
